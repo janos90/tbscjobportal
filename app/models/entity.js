8f7826ac1f3cebe84e4138356053a8674e54286b
Juan <thekingliveson@gmail.com>
2020-10-23 11:12:08 +1300

    added simple auth

---
+++ app/models/entity.js
@@ -0,0 +1,7 @@
+  import Model, { attr } from '@ember-data/model';
+  export default class JobModel extends Model {
+    @attr name;
+    @attr provides;
+    @attr suppliers;
+    @attr forms;
+  }
