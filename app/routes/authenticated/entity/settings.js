9ea0d99d0cc867d26e73ed138c63f18e37219612
Juan <thekingliveson@gmail.com>
2020-10-29 07:20:37 +1300

    tidy settings and added save stub

--- app/routes/authenticated/entity/settings.js
+++ app/routes/authenticated/entity/settings.js
@@ -1,9 +1,22 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class SettingsRoute extends Route {
   @service store;
   async model(params) {
-    return this.store.findRecord('user', '1');
+    return this.store.findRecord('user', '20');
+  }
+
+  @action
+  saveUser() {
+    let self = this;
+    model.userName = this.get('controller').get('userName')
+    model.password = this.get('controller').get('password')
+    model.firstName = this.get('controller').get('firstName')
+    model.lastName = this.get('controller').get('lastName')
+    model.save().then(function() {
+      self.transitionTo('authenticated.jobs');
+    })
   }
 }
