43ae08a0739d51b22d0df171b4984e714e9c0058
Juan <garcij07@myunitec.ac.nz>
2020-10-30 15:20:55 +1300

    fixed logout button

--- app/routes/authenticated/entity.js
+++ app/routes/authenticated/entity.js
@@ -1,10 +1,18 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+
+
 
 export default class EntityRoute extends Route {
   @service store;
   async model(params) {
     return this.store.findRecord('entity',params.entity_id );
+  }
+  
+  @action
+  logout() {
+    this.transitionTo('login');
   }
 
   isAdmin() {
