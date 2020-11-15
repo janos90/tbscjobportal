44f5f60c334db458a16a71922ef726a82b98f59e
Juan <garcij07@myunitec.ac.nz>
2020-11-15 19:12:28 +1300

    added mirage, updated login test

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -5,7 +5,12 @@ import { module, test } from 'qunit';
 module('Acceptance | login', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('testing /login', async function(assert) {
+  test('visiting /login', async function(assert) {
+    await visit('/login');
+    assert.equal(currentURL(), '/login');
+  });
+
+  test('users cant login without details', async function(assert) {
     await visit('/login');
     await click('.login-button');
 
@@ -13,8 +18,26 @@ module('Acceptance | login', function(hooks) {
     assert.equal(currentURL(), '/login');
   });
 
-  test('visiting /login', async function(assert) {
+  test('users cant login with wrong details', async function(assert) {
     await visit('/login');
+    await fillIn('input#login-username', 'Bad Username');
+    await fillIn('input#login-password', 'Bad Password');
+    await click('.login-button');
+
+    // The user is not redirected
     assert.equal(currentURL(), '/login');
   });
+
+  test('users can login with correct details', async function(assert) {
+    await visit('/login');
+    await fillIn('input#login-username', 'good Username');
+    await fillIn('input#login-password', 'good Password');
+    await click('.login-button');
+
+    // The user is not redirected
+    assert.equal(currentURL(), '/authenticated/entities');
+  });
+
+
+
 });
